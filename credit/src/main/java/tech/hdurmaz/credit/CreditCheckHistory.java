39cc7ed7bfe7afaa98ae46238eb60ac0fdc481c0
Hakan Durmaz <durmazhakan@icloud.com>
2023-02-04 15:57:17 +0300

    - reformatted

--- credit/src/main/java/tech/hdurmaz/credit/CreditCheckHistory.java
+++ credit/src/main/java/tech/hdurmaz/credit/CreditCheckHistory.java
@@ -1,15 +1,12 @@
 package tech.hdurmaz.credit;
 
-import java.util.Date;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Builder
@@ -20,12 +17,12 @@ public class CreditCheckHistory {
 
     @Id
     @SequenceGenerator(
-        name = "credit_id_sequence",
-        sequenceName = "credit_id_sequence"
+            name = "credit_id_sequence",
+            sequenceName = "credit_id_sequence"
     )
     @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "credit_id_sequence"
+            strategy = GenerationType.SEQUENCE,
+            generator = "credit_id_sequence"
     )
     private Integer id;
     private String customerId;
